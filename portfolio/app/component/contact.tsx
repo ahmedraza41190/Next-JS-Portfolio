9836d6a138e003ebe8b4aa239cf2be664b5227db
Ahmed Raza <ragix.aemi@gmail.com>
2023-09-09 14:52:14 -0700

    send message is not complete

--- portfolio/app/component/contact.tsx
+++ portfolio/app/component/contact.tsx
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { FormEvent, useState } from "react";
+
 
 const Contact = () => {
+
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [message, setMessage] = useState('')
+
+    const onSubmit = (e: FormEvent) => {
+        e.preventDefault()
+        console.log('data', name, email, message)
+    }
+
     return (
         <div id="contact">
             <section className="text-gray-600 body-font relative ">
@@ -43,42 +54,51 @@ const Contact = () => {
                         <h2 className="sm:text-4xl text-3xl font-[cursive,Comforter] text-[#fff] m-5 bg-initial drop-shadow-[2px_2px_2px_red]">
                             Contact
                         </h2>
-                        <div className="relative mb-4">
+                        <form onSubmit={onSubmit} className="relative mb-4">
+
                             <label htmlFor="name" className="leading-7 text-sm text-white">
-                                Name
+                                Name:
                             </label>
+
                             <input
+                                value={name}
+                                onChange={(e) => setName(e.target.value)}
                                 type="text"
                                 id="name"
                                 name="name"
                                 className="w-full bg-white rounded border border-gray-300 focus:border-blue-500 focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                             />
-                        </div>
-                        <div className="relative mb-4">
+
                             <label htmlFor="email" className="leading-7 text-sm text-white">
-                                Email
+                                Email:
                             </label>
+
                             <input
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
                                 type="email"
                                 id="email"
                                 name="email"
                                 className="w-full bg-white rounded border border-gray-300 focus:border-blue-500 focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                             />
-                        </div>
-                        <div className="relative mb-4">
+
                             <label htmlFor="message" className="leading-7 text-sm text-white">
-                                Message
+                                Message:
                             </label>
+
                             <textarea
+                                value={message}
+                                onChange={(e) => setMessage(e.target.value)}
                                 id="message"
                                 name="message"
                                 className="w-full bg-white rounded border border-gray-300 focus:border-blue-500 focus:ring-2 focus:ring-blue-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
                                 defaultValue={""}
                             />
-                        </div>
-                        <button className="text-white bg-blue-500 border-0 py-2 px-6 focus:outline-none hover:bg-blue-600 rounded text-lg">
-                            Send Message
-                        </button>
+
+                            <button className="text-white bg-blue-500 border-0 py-2 px-6 focus:outline-none hover:bg-blue-600 rounded text-lg">
+                                Send Message
+                            </button>
+                        </form>
                     </div>
                 </div>
             </section>
